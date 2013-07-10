2d38994606c37f9f3cc04ccaa3b453cbfc9f99e3
Ivan <moscoso@gmail.com>
2013-07-09 20:56:52 -0500

    adding scoring output

--- CardGame/Controllers/CardGameViewController.m
+++ CardGame/Controllers/CardGameViewController.m
@@ -13,6 +13,7 @@
 @interface CardGameViewController ()
 @property (nonatomic) NSUInteger count;
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (strong, nonatomic) CardMatchingGame *game;
 @end
@@ -44,6 +45,7 @@
         cardButton.enabled = !card.isUnPlayable;
         cardButton.alpha = card.isUnPlayable ? 0.3 : 1.0;
     }
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.game.score];
 }
 
 -(void)setCardButtons:(NSArray *)cardButtons
